56a5002004bd6939492f426594fec7378490d3d3
saisathwik22 <reddysaisathwik022@gmail.com>
2026-02-02 14:27:43 +0530

    changes

--- app/layout.js
+++ app/layout.js
@@ -7,7 +7,7 @@ import { Toaster } from "sonner";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Welth",
+  title: "fund flow",
   description: "One stop Finance Platform",
 };
 
@@ -15,18 +15,16 @@ export default function RootLayout({ children }) {
   return (
     <ClerkProvider>
       <html lang="en">
-        <head>
+        {/* <head>
           <link rel="icon" href="/logo-sm.png" sizes="any" />
-        </head>
+        </head> */}
         <body className={`${inter.className}`}>
           <Header />
           <main className="min-h-screen">{children}</main>
           <Toaster richColors />
 
           <footer className="bg-blue-50 py-12">
-            <div className="container mx-auto px-4 text-center text-gray-600">
-              <p>Made with 💗 by RoadsideCoder</p>
-            </div>
+            
           </footer>
         </body>
       </html>
